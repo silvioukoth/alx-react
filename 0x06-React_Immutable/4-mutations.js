da4b332cb6a088c0dbe66baef5594b61e8ec5496
silvioukoth <silvioukoth@gmail.com>
2023-05-13 10:14:38 +0000

    add changes

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,14 +1,14 @@
-import { map } from 'immutable';
+import { Map } from 'immutable';
 
-export const map = Map({map 
-	1: 'Liam',
-	2: 'Noah',
-	3: 'Elijah',
-	4: 'Oliver',
-	5: 'Jacob',
-	6: 'Lucas',
-})
+export const map = new Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas'
+});
 
-export const map2 = map.withMutations((values) => {
-	values.set(2, 'Benjamin').set(4, 'Oliver');
+export const map2 = map.withMutations((itemsToBeMapped) => {
+  itemsToBeMapped.set(2, 'Benjamin').set(4, 'Oliver');
 });
